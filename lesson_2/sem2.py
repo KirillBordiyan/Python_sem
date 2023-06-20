8a5acf099b6545a42e47d8bd8924c05f050c7522
Kirill Bordiyan <kirillbordiyan@gmail.com>
2023-06-20 20:53:43 +0300

    sem 2 task 11

--- lesson_2/sem2.py
+++ lesson_2/sem2.py
@@ -12,12 +12,53 @@ def task9():
     i = 1
     if n == 0:
         return f
+    elif n < 0:
+        return -f
     else:
         while i <= n:
-            f = f * i
+            f *= i
             i+=1
         return f
         
-print(task9())
+# print(task9())
 
 
+# Задача №11. Решение в группах
+# Дано натуральное число A > 1. Определите, каким по
+# счету числом Фибоначчи оно является, то есть
+# выведите такое число n, что φ(n)=A. Если А не
+# является числом Фибоначчи, выведите число -1.
+# Input: 5
+# Output: 6
+
+# начинаем 0 1, а дальше счет
+def task11():
+    
+    f0 = 0
+    f1 = 1
+    temp = 0
+    count = 2
+    
+    a = int(input())
+    
+    if a <= 0:
+        return -1
+    elif a == 0 or a == 1:
+        return f"{a+1} and 3"
+    
+    while temp <= a:
+        temp = f0 + f1
+        f0 = f1
+        f1 = temp
+        count += 1
+        if temp == a:
+            return count
+    else:
+        return "not include fed"
+
+        
+# print(task11())
+        
+    
+    
+    
