529064e2552864f3a7bd790549e13af15d055d95
Kirill Bordiyan <kirillbordiyan@gmail.com>
2023-06-22 22:00:42 +0300

    sem 3

--- lk_3/lk2.py
+++ lk_3/lk2.py
@@ -50,21 +50,22 @@ col = {'red', 'blue', 'green', 'black'}
 print(col)
 col.add('white')
 print(col)
+col.add('white')
 col.remove('white')
 print(col)
 col.clear()
 print(col)
 
-a = {1,2,3,4,5,6,7,8,9}
-b = {3,5,7,9,14}
-c = a.copy
-print(c)
-u = a.union(b)
-print(u) #объединяет множества, но значения всегда уникальны (при совпадении не добавляются)
-i = a.intersection(b) #пересечение
-print(i)
-d1 = a.difference(b)#чего нет в а, что есть в b
-d2 = b.difference(a)#чего нет в b, что есть в а
-print(d1)
-print(d2)
+# a = {1,2,3,4,5,6,7,8,9}
+# b = {3,5,7,9,14}
+# c = a.copy
+# print(c)
+# u = a.union(b)
+# print(u) #объединяет множества, но значения всегда уникальны (при совпадении не добавляются)
+# i = a.intersection(b) #пересечение
+# print(i)
+# d1 = a.difference(b)#чего нет в а, что есть в b
+# d2 = b.difference(a)#чего нет в b, что есть в а
+# print(d1)
+# print(d2)
 
