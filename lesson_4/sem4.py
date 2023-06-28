8b356091c338d83686a0ef4e30744d4b7cb23d32
Kirill Bordiyan <kirillbordiyan@gmail.com>
2023-06-28 23:15:40 +0300

    sem 4 task 29

--- lesson_4/sem4.py
+++ lesson_4/sem4.py
@@ -21,7 +21,7 @@ def CharCounter(stroka):
             print(i,end =" ")
         
     
-# CharCounter('a a a b c a a d c d d')
+CharCounter('a a a b c a a d c d d')
 
 
 
@@ -56,7 +56,26 @@ def task27_2(args):
 
 
 args = "She sells sea shells on the sea shore The shells that she sells are sea shells I'm sure.So if she sells sea shells on the sea shore I'm sure that the shells are sea shore shells"
-# task27(args)
-# task27_2(args)
+task27(args)
+task27_2(args)
 
 
+
+
+# 29  “Задана последовательность
+# неотрицательных целых чисел. Требуется определить
+# значение наибольшего элемента
+# последовательности, которая завершается первым
+# встретившимся нулем (число 0 не входит в
+# последовательность)”
+
+def task29():
+    n = int(input())
+    maxNum = n
+    while n != 0:
+        n = int(input())
+        if n > maxNum:
+            maxNum = n
+    print(maxNum)
+    
+task29()
