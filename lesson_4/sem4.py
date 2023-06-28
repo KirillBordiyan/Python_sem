429337a8b928de012bd2429c1da39f6b834e9c94
Kirill Bordiyan <kirillbordiyan@gmail.com>
2023-06-28 22:36:34 +0300

    sem 4 task 27

--- lesson_4/sem4.py
+++ lesson_4/sem4.py
@@ -21,4 +21,42 @@ def CharCounter(stroka):
             print(i,end =" ")
         
     
-CharCounter(input())
+# CharCounter('a a a b c a a d c d d')
+
+
+
+# Задача №27. Решение в группах
+# Пользователь вводит текст(строка). Словом считается
+# последовательность непробельных символов идущих
+# подряд, слова разделены одним или большим числом
+# пробелов. Определите, сколько различных слов
+# содержится в этом тексте.
+
+# Input: She sells sea shells on the sea shore The shells
+# that she sells are sea shells I'm sure.So if she sells sea
+# shells on the sea shore I'm sure that the shells are sea
+# shore shells
+# Output: 13
+
+
+def task27(args):
+    new = args.lower().split()
+    result = {}
+    
+    for i in new:
+        if i in result:
+            continue
+        else:
+            result[i] = 0
+    print(len(result))
+
+def task27_2(args):
+    print(len(set(args.lower().split())))
+
+
+
+args = "She sells sea shells on the sea shore The shells that she sells are sea shells I'm sure.So if she sells sea shells on the sea shore I'm sure that the shells are sea shore shells"
+# task27(args)
+# task27_2(args)
+
+
