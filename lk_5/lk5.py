0f09a5580407365301c1e926fd8111f6d5d7c9a1
Kirill Bordiyan <kirillbordiyan@gmail.com>
2023-07-13 17:06:56 +0300

    lk 5

---
+++ lk_5/lk5.py
@@ -0,0 +1,2 @@
+# https://drive.google.com/open?id=16u60LC8H4drYEGvgfJ-kNRtymF3_n5PR&usp=drive_copy - colab google
+# https://drive.google.com/open?id=1SC1vBy-Cz2hZyjimUr2QIHThk5B6zD9X&usp=drive_copy - pres
